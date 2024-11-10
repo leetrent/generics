0aabd31127e44ba2baede7a31afbc754320711b1
Lee Trent <lee.trent.1@gmail.com>
2024-11-10 11:36:03 -0600

    got dual types working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use num_traits::{Float};
 
-fn solve<T: Float>(a: T, b: T) -> f64 {
+fn solve_with_same_type<T: Float>(a: T, b: T) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+fn solve_with_different_type<T: Float, U: Float>(a: T, b: U) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
@@ -9,10 +16,25 @@ fn solve<T: Float>(a: T, b: T) -> f64 {
 
 
 fn main() {
+    same_type();
+    different_type();
+}
+
+fn same_type() {
     let a_f32: f32 = 3.0;
     let b_f32: f32 = 4.0;  
 
     println!();
-    println!("hypotenuse: {}", solve::<f32>(a_f32, b_f32));
+    println!("[same_type] => (hypotenuse): {}", solve_with_same_type::<f32>(a_f32, b_f32));
     println!();
 }
+
+fn different_type() {
+    let a_f32: f32 = 3.0;
+    let b_f64: f64 = 4.0;  
+
+    println!();
+    println!("[different_type] => (hypotenuse): {}", solve_with_different_type::<f32, f64>(a_f32, b_f64));
+    println!();
+}
+
