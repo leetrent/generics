e4af4abb3f05cc62e70538074d28401b22e4483d
Lee Trent <lee.trent.1@gmail.com>
2024-11-10 10:07:06 -0600

    Completed task

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use num_traits::{Float};
+trait Vehicle {
+    // abstract method
+    fn start(&self);
 
-fn solve<T: Float>(a: T, b: T) -> f64 {
-    let a_f64 = a.to_f64().unwrap();
-    let b_f64 = b.to_f64().unwrap();
-
-    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+    // default method
+    fn stop(&self) {
+        println!("Vehicle stopped");
+    }
 }
 
+struct Car {}
+
+impl Vehicle for Car {
+    fn start(&self) {
+        println!("Car started.")
+    }
+}
+
+fn start_stop<T: Vehicle>(vehicle: T) {
+    vehicle.start();
+    vehicle.stop();
+}
 
 fn main() {
-    let a_f32: f32 = 3.0;
-    let b_f32: f32 = 4.0;  
-
-    println!();
-    println!("hypotenuse: {}", solve::<f32>(a_f32, b_f32));
-    println!();
+    let car = Car{};
+    start_stop(car);
 }
